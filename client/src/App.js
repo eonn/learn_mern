e936aba7f07f9bac628e4eafde5ecdd5bb19230b
eonn <himanshu.she@gmail.com>
2023-07-28 00:55:06 +0530

    Update App to include interfacePage

--- client/src/App.js
+++ client/src/App.js
@@ -5,16 +5,16 @@ import { Route, Routes } from 'react-router-dom'
 // We will create these two pages in a moment
 
 import HomePage from './pages/HomePage';
-import UserPage from './pages/UserPage';
 import RegisterUser from './pages/RegisterUser';
+import InterfacePage from './pages/InterfacePage';
 
 export default function App() {
 
 return (
 	<Routes>
 		<Route exact path="/" element={ <HomePage/> } />
-		<Route path="/:id" element={ <UserPage/> } />
 		<Route path="/Register" element={ <RegisterUser/> } />
+		<Route path="/Interface" element={ <InterfacePage/> } />
 	</Routes>
 )
 }
---
+++ client/src/pages/InterfacePage.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+...
+
+import AddInterface from "./../components/AddInterface";
+import Interface from "./../components/Interface";
+import InterfacesList from "./../components/InterfacesList";
+
+function InterfacePage() {
+  return (
+    <div>
+      <nav className="navbar navbar-expand navbar-dark bg-dark">
+        <a href="/interfaces" className="navbar-brand">
+          bezKoder
+        </a>
+        <div className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <Link to={"/interfaces"} className="nav-link">
+              Interfaces
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to={"/add"} className="nav-link">
+              Add
+            </Link>
+          </li>
+        </div>
+      </nav>
+
+      <div className="container mt-3">
+        <Routes>
+          <Route exact path={["/", "/interfaces"]} element={InterfacesList} />
+          <Route exact path="/add" element={AddInterface} />
+          <Route path="/interfaces/:id" element={Interface} />
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
+export default InterfacePage;
